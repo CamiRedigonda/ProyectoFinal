158308761284b4c7f2faa64f3c30f7561a78aff4
CamiRedigonda <camiredigonda@gmail.com>
2020-04-15 22:16:23 -0300

    Primer commit

---
+++ www/js/my-app.js
@@ -0,0 +1,79 @@
+  
+// If we need to use custom DOM library, let's save it to $$ variable:
+var $$ = Dom7;
+
+var app = new Framework7({
+    // App root element
+    root: '#app',
+    // App Name
+    name: 'My App',
+    // App id
+    id: 'com.myapp.test',
+    // Enable swipe panel
+    panel: {
+      swipe: 'left',
+    },
+    // Add default routes
+    routes: [
+      {
+        path: '/about/',
+        url: 'about.html',
+      },
+    ]
+    // ... other parameters
+  });
+
+var mainView = app.views.create('.view-main');
+
+// Handle Cordova Device Ready Event
+$$(document).on('deviceready', function() {
+    console.log("Device is ready!");
+
+    $$('.tocaBoton').on('click', fnTocaBoton);
+
+});
+
+// Option 1. Using one 'page:init' handler for all pages
+$$(document).on('page:init', function (e) {
+    // Do something here when page loaded and initialized
+    console.log(e);
+})
+
+// Option 2. Using live 'page:init' event handlers for each page
+$$(document).on('page:init', '.page[data-name="about"]', function (e) {
+    // Do something here when page with data-name="about" attribute loaded and initialized
+    console.log(e);
+    alert('Hello');
+})
+
+
+/** FUNCIONES PROPIAS **/
+function fnTocaBoton() {
+    var mensaje = "";
+
+    idDelBoton = this.id;
+    mensaje = "Mi ID es: " + idDelBoton + "<br/>";
+
+    // voy a "separar" el valor del id, usando los guiones bajos
+    // el split separa un valor (en este caso una variable),
+    // usando el caracter o caracteres indicandos como parámetro
+    // el resultado es un array!
+    var partes = idDelBoton.split("_");
+
+    // sabiendo la forma: btn_g1_1 puedo tener:
+    p0 = partes[0];
+    p1 = partes[1];
+    p2 = partes[2];
+
+    mensaje += "Soy del Grupo: " + p1 + "<br/>";
+    mensaje += "Y tengo el nro: " + p2 + "<br/>";
+
+
+    $$('#msgBtn').html(mensaje);
+
+
+
+}
+
+
+
